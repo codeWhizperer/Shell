633b28396d46fce509a0abb94515fa9ceee7e3dc
Adegbite Ademola Kelvin <adegbiteademolakelvin@Adegbites-MacBook-Pro.local>
2024-12-28 17:26:13 +0100

    feat: implement REPL (READ-EVAL-PRINT LOOP)

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,22 @@
 use std::io::{self, Write};
 
 fn main() {
-    // Uncomment this block to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        // Uncomment this block to pass the first stage
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    // Wait for user input
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
-    println!("{}: command not found", input.trim());
+        // Wait for user input
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
+        let command = input.trim();
+
+        // REPL: READ-EVAL-PRINT LOOP
+
+        // To properly implement REPL: I have to implement a loop.
+        match command {
+            _ => println!("{}: command not found", command),
+        }
+    }
 }
