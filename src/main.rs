5f8b62fa7d91de9cc17f3afbe1097f439e297886
Adegbite Ademola Kelvin <adegbiteademolakelvin@Adegbites-MacBook-Pro.local>
2024-12-28 18:05:33 +0100

    feat: add echo implementation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
-use std::{os::unix::process, process::ExitCode};
+use std::{env::args, os::unix::process, process::ExitCode};
 
 fn main() {
     loop {
@@ -12,14 +12,25 @@ fn main() {
         let stdin = io::stdin();
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
-        let command = input.trim();
+        // let command = input.trim();
+        let trimmed = input.trim();
 
         // REPL: READ-EVAL-PRINT LOOP
 
         // To properly implement REPL: I have to implement a loop.
+
+        let mut parts = trimmed.split_whitespace();
+        let command = parts.next().unwrap_or("");
+
         match command {
             "exit 0" => std::process::exit(0),
-            _ => println!("{}: command not found", command),
+            "echo" => {
+                let args: Vec<&str> = parts.collect();
+                println!("{}", args.join(" "))
+            }
+            _ => {
+                println!("{}: command not found", command)
+            }
         }
     }
 }
